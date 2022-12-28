43b71eb2b7568b3d4a50cf9d8ffaae9f2aecc557
NubScript7 <112324399+NubScript7@users.noreply.github.com>
2022-12-29 07:36:05 +0800

    Update mobileNotification.js

--- mobile/mobileNotification.js
+++ mobile/mobileNotification.js
@@ -1,9 +1,10 @@
 function notify(title,body){
+console.log(body)
 navigator.serviceWorker.register('sw.js');
 Notification.requestPermission(perm=>{
-	if(perm==='granted'){
-		if(typeof body !== 'object')return
-		if(typeof body !== 'string'){
+	if(perm=='granted'){
+		if(typeof body != 'object')return
+		if(typeof body != 'string'){
 			navigator.serviceWorker.ready.then(reg => {
 				reg.showNotification(title)
 			})
