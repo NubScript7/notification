4b4a74a0c54388f8feac674810f9691905475ac8
NubScript7 <112324399+NubScript7@users.noreply.github.com>
2022-12-29 07:31:16 +0800

    Add files via upload

---
+++ mobile/mobileNotification.js
@@ -0,0 +1,15 @@
+function notify(title,body){
+navigator.serviceWorker.register('sw.js');
+Notification.requestPermission(perm=>{
+	if(perm==='granted'){
+		if(typeof body !== 'object')return
+		if(typeof body !== 'string'){
+			navigator.serviceWorker.ready.then(reg => {
+				reg.showNotification(title)
+			})
+		}else{
+		navigator.serviceWorker.ready.then(reg=>{
+			reg.showNotification(title,body)
+		})}
+	}
+})}
